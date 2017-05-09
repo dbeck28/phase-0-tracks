dfd23ddf53b08a05803a73f5e4cb89e6930a9525
Dayne Beck <daynebeck28@gmail.com>
2017-05-09 11:00:25 -0500

    Working responses for all release 2 conditionals

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,38 +1,106 @@
+employees = 0
+until employees * 1 > 0
+puts "How many employees will be processed?(enter a number)"
+  employees = gets.chomp.to_i
+end
+
+
+for i in (1..employees)
+
 puts "Enter Your Name:"
   your_name = gets.chomp
-    sleep 1 # 1 sec pause, before the next question
 puts "Ok #{your_name}, how old are you?"
   age = gets.chomp.to_i
-    sleep 1
 puts "You're #{age} huh? What year were you born?"
   year_of_birth = gets.chomp.to_i
-    sleep 1
 
-bread = ""
-until bread == "yes" or bread == "no"
+bread = false
+user = ""
+until user == "yes" or user == "no"
   puts "Ahhhhh #{year_of_birth}. We serve garlic bread in the cafe. It's really good. Would you like to order some? (answer: yes or no)"
-  bread = gets.chomp
+  user = gets.chomp
 end
 
-medical = ""
-until medical == "yes" or medical == "no"
+if user == "yes"
+  bread = true
+end
+
+medical = false
+user_2 = ""
+until user_2 == "yes" or user_2 == "no"
   puts "You want to enroll in our medical plan? (answer: yes or no)"
-  medical = gets.chomp
+  user_2 = gets.chomp
 end
 
-if (your_name == "Drake Cula") or (your_name == "Tu Fang")
-  abort ("Definitely a vampire") #abort to end program regardless of other conditions
+if user_2 == "yes"
+  medical = true
 end
 
+age_x = Time.new.year.to_i
+responses = []
+age_right = false
 
-if (Time.new.year - year_of_birth == age or age + 1) && (bread == "yes") && (medical == "yes")
-  puts "Probably not a vampire"
-elsif (Time.new.year - year_of_birth != age or age + 1) && (bread == "no") or (medical == "no")
-  puts "Probably a vampire"
-elsif (Time.new.year - year_of_birth != age or age + 1) && (bread == "no") && (medical == "no")
-  puts "Almost Certainly A Vampire"
-else
+if (your_name == "Drake Cula") || (your_name == "Tu Fang")
+  a = "definitely a vampire"
+  puts a
+  responses.push(a)
+end
+
+if (age_x - year_of_birth != age) && (age_x -year_of_birth != (age + 1))
+  x = true
+  if x && medical == false && bread == false
+    b = "Almost Certainly a vampire."
+    puts b
+    responses.push(b)
+  end
+end
+
+if (age_x - year_of_birth == age) || (age_x - year_of_birth == (age + 1))
+  age_right = true
+  if age_right && (medical or bread) && (your_name != "Drake Cula") && (your_name != "Tu Fang")
+    c = "Probably not a vampire."
+    puts c
+    responses.push(c)
+  end
+end
+
+if (age_x - year_of_birth != age) && (age_x - year_of_birth != (age + 1))
+  age_right = false
+  if ((age_right == false) && (medical == false && bread == true)) || ((age_right == false) && (medical == true && bread == false))
+    d = "Probably a vampire."
+    puts d
+    responses.push(d)
+  end
+end
+
+if responses == []
   puts "results inconclusive"
 end
+end
+
+=begin
+age_x = Time.new.year.to_i
+unless (your_name == "Drake Cula") or (your_name == "Tu Fang")
+  if (age_x - year_of_birth == age) || (age_x - year_of_birth == (age + 1))
+    age = 0
+    if age == 0 && (bread || medical == "yes")
+  puts "Probably not a vampire"
+  end
+  elsif (age_x - year_of_birth != age) || (age_x - year_of_birth != (age + 1))
+    age = 1
+    if age == 1 && (bread || medical == "no")
+  puts "Probably a vampire"
+  end
+  elsif (age_x - year_of_birth != age) || (age_x - year_of_birth != (age + 1))
+    age = 1
+    if age == 1 && (bread && medical == "no")
+  puts "Almost Certainly A Vampire"
+  end
+  else
+  puts "results inconclusive"
+end
+end
+end
+=end
 
 
