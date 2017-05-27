af510a56364b685d605f02dc29361c667bb8e795
Dayne Beck <daynebeck28@gmail.com>
2017-05-27 17:24:22 -0500

    yell angrily and happily methods added to shout moule

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,2 +1,12 @@
 module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    words + "!$!" + " :D"
+  end
 end
+
+p Shout.yelling_happily("I saw a bird")
+p Shout.yell_angrily("I dislike birds")
