3cb8335a75d1321c119ef2180c21f89c48998c1d
Dayne Beck <daynebeck28@gmail.com>
2017-05-27 17:36:10 -0500

    Shout.rb updated

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
@@ -10,3 +11,28 @@ end
 
 p Shout.yelling_happily("I saw a bird")
 p Shout.yell_angrily("I dislike birds")
+=end
+
+module Shout
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yelling_happily(words)
+    words + "!$!" + " :D"
+  end
+end
+
+class Singer
+  include Shout
+end
+
+class Coach
+  include Shout
+end
+
+beyonce = Singer.new
+p beyonce.yelling_happily("I don't think you ready for this jelly!")
+
+phil = Coach.new
+p phil.yell_angrily("Guard Him")
