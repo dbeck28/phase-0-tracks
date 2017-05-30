8b346020dd1412787b62e6d3eaedf9f56254c5cb
Dayne Beck <daynebeck28@gmail.com>
2017-05-30 00:42:38 -0500

    game class and spec added

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,45 +1,80 @@
 
 class Game
-# a method to accept the answer to be guessed
-# input: the answer (string) by one user to be guessed by another user
-#steps:
-  # Create a method that takes one word
-  # Save and set that word as the answer
-# output: the answer
-  def answer(word)
+  attr_accessor :revealer
+  attr_accessor :counter
+  attr_accessor :answer
+  attr_accessor :guesses
+
+  def initialize
+    @counter = 0
+    @revealer = []
+    @is_over = false
+    @answer = ""
+    @guesses = []
+  end
+
+  def guess_answer(word)
     @answer = word
     p @answer
   end
 
+  def revealer
+   @revealer = ("_" * (@answer.length)).split("")
+   p @revealer.join
+  end
+
+  def guess_counter(guess)
+    @guesses << guess
+    if @guesses.count(guess) == 1 #Only adds to counter if this is the first time the guess has been made
+      @counter += 1
+    end
+    @guess = guess
+    p @guesses
+    p @counter
+  end
+
+  def rev_update
+    @answer.split("").index.each do |letter|
+      if (letter == @guess) && (@revealer[@answer.index(@guess)] == "_")
+        @revealer.delete_at(@answer.index(@guess))
+        @revealer.insert(@answer.index(@guess), @guess)
+      end
+    end
+    p @revealer.join
+  end
+
+  def final(word)
+    if word == @answer
+      puts "Congrats! You guessed it in #{@counter.to_s} tries!"
+    end
+    if (guesses.length > @answer.length) && (word != @answer)
+      puts "No dude, you're a loser"
+    end
+  end
+
 end
 
-# initialize a new instance of the game
+puts "New Game? (Y/N)"
+new_game = gets.chomp
 
-# a method to accept the answer to be guessed
-# input: the answer (string) by one user to be guessed by another user
-#steps:
-  # Create a method that takes one word
-  # Save and set that word as the answer
-# output: the answer
+if new_game == "y"
+  choco = Game.new
+end
 
-# a method to limit the amount of attempts a user can guess
-# input: the length of the answer
-# steps:
-  # Allow the user to guess the answer as a whole or one letter at a time for as many times as the length of the word, setting a counter to keep track
-  # For each guess, add it to an array of guesses, and add 1 to the counter...unless it is a guess that already exists in the array.
-  # If it is a word in the array, give the user a message that it was previously guessed.
-# output: an array of guessed words
+p "Player 1, enter a word to be guessed"
+@answer = choco.guess_answer(gets.chomp)
+choco.revealer
 
-# a method to display the state of the word to be guessed. If letters are guessed 1 at a time, display to the user the length of the word as "_"'s, with the letter guessed filled in. (ex: for the word "theme", if you guess e, you'll get __e_e.)
-# input: the letter guessed, the answer
-# steps:
-  # for the length of the word, create a new string that is the length of the word using "_".
-  # for the letter guessed, enter it in place into the new string.
-# output: the new string after the guess
+until choco.counter > @answer.length
+  p "Player 2, make a guess:"
+  choco.guess_counter(gets.chomp)
+  choco.rev_update
+end
 
-# a method that displays a congratulatory message if the user guesses the answer, or an antagonizing message if the max amount of guesses is reached.
-# input: the answer, the counter
-# steps:
-  # if the answer is guessed, display a congratulatory message
-  # if the counter is maxed out, display a taunting message
-# output: a message
+p "One last try, what's your answer?"
+choco.final(gets.chomp)
+
+
+
+
+
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -9,4 +9,14 @@ describe Game do
   it "takes a word and sets it as the answer" do
     expect(Game.new.answer("green")).to eq "green"
   end
+
+  it "counts the amount of guesses and adds to the counter" do
+    expect(Game.new.guess_counter("r")).to eq 1
+  end
+
+  it "gives an update of the status of guesses" do
+    expect(Game.new.rev_update).to eq ""
+  end
+
+
 end
