211273f5b052de41dcd581123b2256ac1f845441
Dayne Beck <daynebeck28@gmail.com>
2017-06-05 07:41:40 -0500

    explore.js pseudocode added

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+// Convert the string to an array by using the split method
+// Use the reverse method on that array
+// Use the join method to convert the reversed array to a string
+// return the string
+
