e5a0230275686faf0771c5b89bc7073d8f5e1289
Dayne Beck <daynebeck28@gmail.com>
2017-06-05 10:22:07 -0500

    function to check if objects share value pairs completed

--- js/algos.js
+++ js/algos.js
@@ -1,16 +1,46 @@
-// Set variables to compare string lengths and to hold the longest word
-// for each value in the array, compare the length and set the longest value if the word is longer than the previous longest value
-// Print the word if it is the longest value
-function longword(arr) {
+// Set variables to compare string lengths and to hold the longest phrase
+// for each value in the array, compare the length and set the longest value if the phrase is longer than the previous longest value
+// Print the phrase if it is the longest value
+
+function longphrase(arr) {
   var longest = 0
   var answer = ""
   for (var i = 0; i < arr.length; i++) {
     if (arr[i].length > longest) {
       var longest = arr[i].length;
-      var answer = arr[i]
+      var answer = arr[i];
     }
   }
   console.log(answer);
 }
 
-console.log(longword(["apple", "orange", "nana"]))
+// Compare the keys of both values to assure they are the same
+// If the keys are the same
+  // Assign the values to seperate variables
+// If the given values of the keys are the same
+// return true
+function sharevalue(obj1, obj2) {
+  var x = Object.keys(obj1);
+  var y = Object.keys(obj2);
+  var a = ""
+  var b = ""
+  if ((x[0] === y[0]) && (x[1] === y[1])) {
+    var a = Object.keys(obj1).map(function(key) {
+      return obj1[key];
+    });
+    var b = Object.keys(obj2).map(function(key) {
+      return obj2[key];
+    });
+  }
+  if ((a[0]) === b[0] || (a[1] === b[1])) {
+    console.log(true);
+  }
+  if ((a[0]) != b[0] && (a[1] != b[1])) {
+    console.log(false);
+  }
+}
+
+console.log(longphrase(["apple", "orange", "nana"]))
+
+console.log(sharevalue({name: "Steven", age: 54}, {name: "Tamir", age: 54}))
+
