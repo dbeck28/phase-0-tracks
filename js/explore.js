995f1da07ea1fa416149651908370506661bf44b
Dayne Beck <daynebeck28@gmail.com>
2017-06-05 07:49:42 -0500

    Explore.js complete

--- js/explore.js
+++ js/explore.js
@@ -3,3 +3,14 @@
 // Use the join method to convert the reversed array to a string
 // return the string
 
+function reversestr(str) {
+  var orig_str = str.split("");
+  var new_str = orig_str.reverse().join("");
+  return new_str;
+}
+
+var answer = reversestr("Yolo");
+
+if (6 == (2*3)) {
+console.log(answer);
+}
